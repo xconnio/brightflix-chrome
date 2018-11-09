ca49fda40434d56215d2e9f36a3d3eb75f92a7ac
Omer Akram <om26er@gmail.com>
2018-11-09 23:32:36 +0500

    Minor code improvements

--- background.js
+++ background.js
@@ -1,19 +1,37 @@
+const NETFLIX = "https://www.netflix.com";
+
+// TODO: Make these configurable.
+const BRIGHTNESS_RAISED = 50;
+const BRIGHTNESS_LOWERED = 10;
+
+
 function set_brightness(brightness) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://127.0.0.1:5020/brightness", true);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    data = {"brightness": brightness}
-    xhr.send(JSON.stringify(data));
+    var request = new XMLHttpRequest();
+    request.open("POST", "http://127.0.0.1:5020/brightness", true);
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.send(JSON.stringify({"brightness": brightness}));
 }
+
+
+function raise() {
+    set_brightness(BRIGHTNESS_RAISED);
+}
+
+
+function lower() {
+    set_brightness(BRIGHTNESS_LOWERED);
+}
+
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete' && tab.url.startsWith(NETFLIX)) {
+        raise();
+    }
+});
 
 
 chrome.tabs.onActivated.addListener(function(info) {
     chrome.tabs.get(info.tabId, function(tab) {
-        if (tab.url.startsWith("https://www.netflix.com")) {
-            set_brightness(50);
-        } else {
-            set_brightness(10);
-        }
+        tab.url.startsWith(NETFLIX) ? raise(): lower()
     });
 });
-
